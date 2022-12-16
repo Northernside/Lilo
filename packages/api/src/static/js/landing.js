fb034dabd238d026dd9fe2df50d196e78029d4c3
Elias <github@northernsi.de>
2022-12-16 12:48:57 +0100

    👉 Clickable server widget on landing page

--- packages/api/src/static/js/landing.js
+++ packages/api/src/static/js/landing.js
@@ -1,25 +1,27 @@
 document.getElementsByClassName("server")[0].innerHTML = "Loading data...";
 
 let req = new XMLHttpRequest();
-req.open("GET", "https://lilo.northernsi.de/api/featuredServer", true);
+req.open("GET", "http://localhost:3000/api/featuredServer", true);
 
 req.onload = () => {
     const serverData = JSON.parse(req.responseText);
 
     let serverHTML = `
-        <div class="favicon">
-            <img src="{favicon}" width="64px" alt="Server Favicon">
-        </div>
-        <div class="align-right">
-            <div class="server-header">
-                <h3>{server_name}</h3>
-                <p>{player_count}</p>
+        <a href="https://lilo.northernsi.de/server/{server_name}">
+            <div class="favicon">
+                <img src="{favicon}" width="64px" alt="Server Favicon">
             </div>
-            <p class="motd">{motd}</p>
-            <p class="small-info">
-                running on <span>{version}</span> <span>{version_number}</span> with a latency of <span>{latency}</span>
-            </p>
-        </div>`;
+            <div class="align-right">
+                <div class="server-header">
+                    <h3>{server_name}</h3>
+                    <p>{player_count}</p>
+                </div>
+                <p class="motd">{motd}</p>
+                <p class="small-info">
+                    running on <span>{version}</span> <span>{version_number}</span> with a latency of <span>{latency}</span>
+                </p>
+            </div>
+        </a>`;
 
     serverHTML = serverHTML.replace(/{server_name}/g, serverData.server_name);
     serverHTML = serverHTML.replace(/{motd}/g, serverData.motd);
